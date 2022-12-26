d2d254dc10bbd448a42894a27dca08a53917a83f
Mano Sanjay <manosanjay.web@gmail.com>
2022-12-27 02:42:08 +0530

    implemented dummy events

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import ParticlesBackground from "./components/ParticlesBackground";
-
+import EventCard from "./components/EventCard";
 const App = () => {
   const goToTop = () => {
     window.location.href = "#events-technical"
   };
   return (
-    <div className="App">
+    <div className="App bg-black">
       <ParticlesBackground />
 
-      <div id="landing" className="flex flex-col py-7 bg-black text-white min-h-screen  justify-between">
+      <div id="landing" className="flex flex-col py-7  text-white min-h-screen  justify-between">
         <div className="flex flex-row justify-center">
           <img src={require('./assets/REC.png')} className="h-15 w-40" alt="" />
         </div>
         <div className="flex flex-col items-center justify-center text-center">
-          <img src={require('./assets/BootUp_23Logo-White.png')} className="max-w-sm" alt="" />
-          <p className="mt-2 mb-1 tracking-widest font-semibold text-lg">BROUGHT TO YOU BY</p>
-          <p className="font-semibold text-lg tracking-widest">DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</p>
+          <img src={require('./assets/BootUp_23Logo-White.png')} className="max-w-xs md:max-w-md lg:max-w-lg" alt="" />
+          <p className="mt-2 mb-1 tracking-widest font-semibold text-lg ">BROUGHT TO YOU BY</p>
+          <p className="font-semibold text-lg tracking-widest px-2">DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</p>
         </div>
         <div className="flex flex-row justify-center">
           <button onClick={goToTop} className="border-solid border-2 border-white px-4 py-2 w-fit tracking-widest transition duration-700  hover:bg-violet-500 ">
@@ -28,8 +28,17 @@ const App = () => {
           <h1 className="text-4xl tracking-widest">EVENTS</h1>
           <p className="text-gray-400 my-2 text-xl tracking-widest">TECHNICAL</p>
         </div>
-        <div className="flex flex-row justify-center">
-          TECHINCAL EVENT CARDS GO HERE
+        <div className="flex-wrap mx-10 sm:mx-0 justify-center sm:flex gap-10 mt-4">
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
         </div>
 
       </div>
@@ -38,8 +47,17 @@ const App = () => {
           <h1 className="text-4xl tracking-widest">EVENTS</h1>
           <p className="text-gray-400 my-2 text-xl tracking-widest">NON-TECHNICAL</p>
         </div>
-        <div className="flex flex-row justify-center">
-          NON-TECHINCAL EVENT CARDS GO HERE
+        <div className="flex-wrap mx-10 sm:mx-0 justify-center sm:flex gap-10 mt-4">
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
         </div>
       </div>
       <div id="sponsers-and-organizers" className="flex flex-col py-7 bg-black text-white min-h-screen">
