5f4fbad627e2a83b2497a1c29adc75222fa3edac
Mano Sanjay <manosanjay.web@gmail.com>
2022-12-27 02:41:53 +0530

    added event card component

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,6 +1,20 @@
 const EventCard = () => {
     return (
-        <div id="event-card"></div>
+        <div
+            id="event-card"
+            class="flex flex-col items-center justify-between border-4  border-blue-400  bg-opacity-100 text-center mt-4 cursor-default lg:max-w-xs py-6 px-9  "
+        >
+
+            <div className="mb-4 place-self-center">
+                <h2 class="font-semibold text-2xl mb-1 tracking-widest">EVENT NAME</h2>
+                <p className="text-gray-300 font-semibold tracking-widest">EVENT INFO</p>
+            </div>
+            <img src={require("../assets/acmCert.png")} className="w-32 my-2 self-center" alt="event-logo" />
+            <button className="self-center border-solid border-2 mt-5 border-white px-4 py-2 w-fit tracking-widest transition duration-700  hover:bg-blue-400 ">
+                MORE INFO
+            </button>
+
+        </div>
     )
 }
 
