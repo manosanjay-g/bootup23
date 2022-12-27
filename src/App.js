c6ba83edeeef747fb9be7c05c662d20d277b2bad
Mano Sanjay <manosanjay.web@gmail.com>
2022-12-27 21:46:03 +0530

    changed the size of the arrow buttons

--- src/App.js
+++ src/App.js
@@ -246,11 +246,11 @@ const App = () => {
           <p className="text-gray-400 my-2 font-semibold text-xl wide-text">TECHNICAL</p>
         </div>
         <div className="flex mx-2 gap-4 items-center">
-          <img src="https://img.icons8.com/ios/50/ffffff/left-squared--v1.png" className="w-10 h-10 opacity-50 hover:opacity-100 transition duration-700 cursor-pointer hover:scale-150" alt="" onClick={slideLeftTechnical} />
+          <img src="https://img.icons8.com/ios/50/ffffff/left-squared--v1.png" className="w-6 h-6 md:w-10 h-10 opacity-50 hover:opacity-100 transition duration-700 cursor-pointer hover:scale-150" alt="" onClick={slideLeftTechnical} />
           <div id="technical-slider" className="flex px-4 overflow-x-scroll gap-8 scroll scroll-smooth whitespace-nowrap snap-x">
             {technicalEventsMap}
           </div>
-          <img src="https://img.icons8.com/ios/50/ffffff/right-squared--v1.png" className="w-10 h-10 opacity-50 hover:opacity-100 transition duration-700 cursor-pointer hover:scale-150" alt="" onClick={slideRightTechnical} />
+          <img src="https://img.icons8.com/ios/50/ffffff/right-squared--v1.png" className="w-6 h-6 md:w-10 h-10 opacity-50 hover:opacity-100 transition duration-700 cursor-pointer hover:scale-150" alt="" onClick={slideRightTechnical} />
         </div>
 
       </div>
@@ -260,11 +260,11 @@ const App = () => {
           <p className="text-gray-400 my-2 text-xl wide-text">NON-TECHNICAL</p>
         </div>
         <div className="flex mx-2 gap-4 items-center">
-          <img src="https://img.icons8.com/ios/50/ffffff/left-squared--v1.png" className="w-10 h-10 opacity-50 hover:opacity-100 transition duration-700 cursor-pointer hover:scale-150" alt="" onClick={slideLeftNonTechnical} />
+          <img src="https://img.icons8.com/ios/50/ffffff/left-squared--v1.png" className="w-6 h-6 md:w-10 h-10 opacity-50 hover:opacity-100 transition duration-700 cursor-pointer hover:scale-150" alt="" onClick={slideLeftNonTechnical} />
           <div id="non-technical-slider" className="flex px-4 overflow-x-scroll gap-8 scroll scroll-smooth whitespace-nowrap snap-x">
             {nonTechnicalEventsMap}
           </div>
-          <img src="https://img.icons8.com/ios/50/ffffff/right-squared--v1.png" className="w-10 h-10 opacity-50 hover:opacity-100 transition duration-700 cursor-pointer hover:scale-150" alt="" onClick={slideRightNonTechnical} />
+          <img src="https://img.icons8.com/ios/50/ffffff/right-squared--v1.png" className="w-6 h-6 md:w-10 h-10 opacity-50 hover:opacity-100 transition duration-700 cursor-pointer hover:scale-150" alt="" onClick={slideRightNonTechnical} />
         </div>
       </div>
       <div id="sponsers-and-organizers" className="flex flex-col py-10 bg-black text-white ">
