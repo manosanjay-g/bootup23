fa1a06e9e897e7b4eca45548e5f9dc2a1b1799c4
Mano Sanjay <manosanjay.web@gmail.com>
2022-12-27 11:33:15 +0530

    added corner squares to event card

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -5,6 +5,11 @@ const EventCard = () => {
             class="flex flex-col items-center justify-between border-4  border-blue-400  bg-opacity-100 text-center mt-4 cursor-default lg:max-w-xs py-6 px-9  "
         >
 
+            <div class="box top left"></div>
+            <div class="box top right"></div>
+            <div class="box bottom left"></div>
+            <div class="box bottom right"></div>
+
             <div className="mb-4 place-self-center">
                 <h2 class="font-semibold text-2xl mb-1 tracking-widest">EVENT NAME</h2>
                 <p className="text-gray-300 font-semibold tracking-widest">EVENT INFO</p>
