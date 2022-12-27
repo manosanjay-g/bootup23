a1d109ad3e64b53b01e20c2fe82894511cc6ea37
Mano Sanjay <manosanjay.web@gmail.com>
2022-12-27 14:53:23 +0530

    added event icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,160 @@
 import ParticlesBackground from "./components/ParticlesBackground";
 import EventCard from "./components/EventCard";
 const App = () => {
-  const goToTop = () => {
+  const goToTechnical = () => {
     window.location.href = "#events-technical"
   };
+  const technicalEvents = [
+    {
+      name: "Silicon Valley",
+      info: "Project Expo",
+      icon: "https://img.icons8.com/windows/96/ffffff/profitable-idea.png",
+      route: "/silicon-valley",
+      description: "",
+    },
+    {
+      name: "The Revolution",
+      info: "Paper Presentation",
+      icon: "https://img.icons8.com/windows/96/ffffff/presentation-screen.png",
+      route: "/the-revolution",
+      description: "",
+    },
+    {
+      name: "404 Not Found",
+      info: "BUG HUNTING",
+      icon: "https://img.icons8.com/ios/100/ffffff/bug.png",
+      route: "/404-not-found",
+      description: "",
+    },
+    {
+      name: "iCatching",
+      info: "WEB DEVELOPMENT",
+      icon: "https://img.icons8.com/ios/100/ffffff/web.png",
+      route: "/icatching",
+      description: "",
+    },
+    {
+      name: "Halt and Catch Fire",
+      info: "Code Relay",
+      icon: "https://img.icons8.com/ios/100/ffffff/keyboard.png",
+      route: "/halt-and-catch-fire",
+      description: "",
+    },
+    {
+      name: "The Perfect Exploit",
+      info: "Capture the flag",
+      icon: "https://img.icons8.com/ios/100/ffffff/flag--v1.png",
+      route: "/the-perfect-exploit",
+      description: "",
+    },
+    {
+      name: "The Office",
+      info: "Interview",
+      icon: "https://img.icons8.com/ios-filled/100/ffffff/briefcase.png",
+      route: "/the-office",
+      description: "",
+    },
+    {
+      name: "Machine Impossible",
+      info: "PC Assembling",
+      icon: "https://img.icons8.com/ios/100/ffffff/workstation.png",
+      route: "/machine-impossible",
+      description: "",
+    },
+    {
+      name: "Ex Machina",
+      info: "RPA",
+      icon: "https://img.icons8.com/material-rounded/96/ffffff/bot.png",
+      route: "/ex-machina",
+      description: "",
+    },
+    {
+      name: "Fly High",
+      info: "Drone Event",
+      icon: "https://img.icons8.com/material-rounded/96/ffffff/drone.png",
+      route: "/fly-high",
+      description: "",
+    },
+  ]
+  const nonTechnicalEvents = [
+    {
+      name: "#Vanmam",
+      info: "Rant Talk",
+      icon: "https://img.icons8.com/ios-filled/100/ffffff/voice-recognition-scan.png",
+      route: "/vanmam",
+      description: "",
+    },
+    {
+      name: "Ready Player One",
+      info: "(Valorant)",
+      icon: "https://img.icons8.com/ios/100/ffffff/valorant.png",
+      route: "/ready-player-one-valorant",
+      description: "",
+    },
+    {
+      name: "Ready Player One",
+      info: "(BGMI)",
+      icon: "https://img.icons8.com/ios-glyphs/90/ffffff/pubg.png",
+      route: "/ready-player-one-bgmi",
+      description: "",
+    },
+    {
+      name: "Oru Naal President",
+      info: "Presidential Campaign",
+      icon: "https://img.icons8.com/external-others-pike-picture/100/ffffff/external-President-democracy-others-pike-picture-3.png",
+      route: "/oru-naal-president",
+      description: "",
+    },
+    {
+      name: "Fandom Wars",
+      info: "Fandom Quiz",
+      icon: "https://img.icons8.com/ios-filled/100/ffffff/fandom.png",
+      route: "/fandom-quiz",
+      description: "",
+    },
+    {
+      name: "Naduvula Konjam Audio-va Kaanom",
+      info: "Audio Dubbing",
+      icon: "https://img.icons8.com/material-sharp/96/ffffff/micro.png",
+      route: "/naduvula-konjam-audiova-kaanom",
+      description: "",
+    },
+    {
+      name: "Tiki-Taka",
+      info: "Football",
+      icon: "https://img.icons8.com/ios-glyphs/90/ffffff/beach-soccer.png",
+      route: "/tiki-taka",
+      description: "",
+    },
+    {
+      name: "Master the Blaster",
+      info: "Box Cricket",
+      icon: "https://img.icons8.com/ios/100/ffffff/cricket.png",
+      route: "/master-the-blaster",
+      description: "",
+    },
+    {
+      name: "Kodak",
+      info: "Photograph",
+      icon: "https://img.icons8.com/ios-glyphs/90/ffffff/compact-camera.png",
+      route: "/kodak",
+      description: "",
+    },
+    {
+      name: "She Boss!",
+      info: "Exclusive to WOMEN",
+      icon: "https://img.icons8.com/ios-filled/100/ffffff/woman-head.png",
+      route: "/she-boss",
+      description: "",
+    },
+  ]
+
+  const technicalEventsMap = technicalEvents.map((e) => {
+    return <EventCard name={e.name} info={e.info} icon={e.icon} />
+  })
+  const nonTechnicalEventsMap = nonTechnicalEvents.map((e) => {
+    return <EventCard name={e.name} info={e.info} icon={e.icon} />
+  })
   return (
     <div className="App ">
       <ParticlesBackground />
@@ -18,46 +169,28 @@ const App = () => {
           <p className="font-semibold wide-text text-md  px-2">DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</p>
         </div>
         <div className="flex flex-row justify-center">
-          <button id="explore-button" onClick={goToTop} className="border-solid border-2 wide-text border-white px-4 py-2 w-fit tracking-widest transition duration-700  hover:bg-blue-400 ">
+          <button id="explore-button" onClick={goToTechnical} className="border-solid border-2 wide-text border-white px-4 py-2 w-fit tracking-widest transition duration-700  hover:bg-blue-400 ">
             EXPLORE
           </button>
         </div>
       </div>
-      <div id="events-technical" className="flex flex-col py-7 bg-black text-white min-h-screen ">
+      <div id="events-technical" className="flex flex-col py-7 px-6 bg-black text-white min-h-screen ">
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-4xl font-semibold wide-text">EVENTS</h1>
           <p className="text-gray-400 my-2 font-semibold text-xl wide-text">TECHNICAL</p>
         </div>
-        <div className="flex-wrap mx-10 sm:mx-0 justify-center sm:flex gap-10 mt-4">
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
+        <div className="flex-wrap mx-10 sm:mx-0 justify-center sm:grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10  mt-4">
+          {technicalEventsMap}
         </div>
 
       </div>
-      <div id="events-non-technical" className="flex flex-col py-7 bg-black text-white min-h-screen ">
+      <div id="events-non-technical" className="flex flex-col py-7 px-6 bg-black text-white min-h-screen ">
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-4xl wide-text">EVENTS</h1>
           <p className="text-gray-400 my-2 text-xl wide-text">NON-TECHNICAL</p>
         </div>
-        <div className="flex-wrap mx-10 sm:mx-0 justify-center sm:flex gap-10 mt-4">
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
+        <div className="flex-wrap mx-10 sm:mx-0 justify-center sm:grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10  mt-4">
+          {nonTechnicalEventsMap}
         </div>
       </div>
       <div id="sponsers-and-organizers" className="flex flex-col py-7 bg-black text-white min-h-screen">
--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,8 +1,9 @@
-const EventCard = () => {
+const EventCard = ({ name, info, icon }) => {
+
     return (
         <div
             id="event-card"
-            class="bg-gray-800 bg-opacity-60 flex flex-col items-center justify-between border-4  border-blue-400   text-center mt-4 cursor-default lg:max-w-xs py-6 px-9  "
+            class="bg-gray-800  bg-opacity-60 flex flex-col items-center justify-between border-4  border-blue-400   text-center mt-4 cursor-default lg:max-w-xs py-6 px-6"
         >
 
             <div class="box top left"></div>
@@ -11,10 +12,10 @@ const EventCard = () => {
             <div class="box bottom right"></div>
 
             <div className="mb-4 place-self-center">
-                <h2 class="font-semibold text-2xl mb-1 tracking-widest">EVENT NAME</h2>
-                <p className="text-gray-300 font-semibold tracking-widest">EVENT INFO</p>
+                <h2 class="font-semibold text-2xl mb-1 tracking-widest ">{name}</h2>
+                <p className="text-gray-300 font-semibold tracking-widest">{info}</p>
             </div>
-            <img src={require("../assets/acmCert.png")} className="w-32 my-2 self-center" alt="event-logo" />
+            <img alt="event-img" src={icon} className="w-20 my-2 self-center " />
             <button className="self-center border-solid border-2 mt-5 border-blue-400 px-4 py-2 w-fit tracking-widest transition duration-700  hover:bg-blue-400 ">
                 MORE INFO
             </button>
