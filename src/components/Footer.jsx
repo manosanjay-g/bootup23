922aecf13c7fca30f78f1e42cb6bda03ab5cfe4c
manosanjay-g <manosanjay.web@gmail.com>
2023-01-22 18:09:01 +0530

    removed some margins

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <div id="footer" className="flex flex-col md:flex-row w-full p-7 bg-zinc-900 md:justify-start">
             <p className="text-white text-3xl mb-4 font-semibold text-center ">BOOTUP'23</p>
-            <div id="footer-items" className="flex flex-col my-4 gap-y-6 md:flex-row md:justify-evenly md:w-full">
+            <div id="footer-items" className="flex flex-col gap-y-6 md:flex-row md:justify-evenly md:w-full md:m-0">
                 {/* <div className="footer-item-links">
                     <p className="text-white font-semibold  justify-center text-2xl  tracking-widest text-2xl">Links</p>
                     <div className='h-1 w-14 bg-blue-400 mb-5'></div>
