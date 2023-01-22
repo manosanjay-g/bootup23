496afe64ef6113a92b7bf917ceb46d75ccddfe00
Subze23 <subzesubhashsanth@gmail.com>
2023-01-22 12:38:54 +0530

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,13 @@ const Footer = () => {
         <div id="footer" className="flex flex-col md:flex-row w-full p-7 bg-zinc-900 md:justify-start">
             <p className="text-white text-3xl my-4 font-semibold text-center ">Bootup'23</p>
             <div id="footer-items" className="flex flex-col my-4 gap-y-6 md:flex-row md:justify-evenly md:w-full">
-                <div className="footer-item-links">
+                {/* <div className="footer-item-links">
                     <p className="text-white font-semibold  justify-center text-2xl  tracking-widest text-2xl">Links</p>
                     <div className='h-1 w-14 bg-blue-400 mb-5'></div>
                     <p className="text-white text-white text-lg"><a href="/">Home</a></p>
                     <p className="text-white text-lg"><a href="/">Contact Us</a></p>
                     <p className="text-white text-lg"><a href="/">Organizers</a></p>
-                </div>
+                </div> */}
                 <div className="footer-item-events">
                     <p className="text-white font-semibold  justify-center text-2xl  tracking-widest text-2xl">Events</p>
                     <div className='h-1 w-14 bg-blue-400 mb-5'></div>
