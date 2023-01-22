d70605d3516f55f1e3028904c5af726b117e3f54
manosanjay-g <manosanjay.web@gmail.com>
2023-01-22 13:52:33 +0530

    changed to explore

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -924,20 +924,20 @@ const Home = () => {
                     </p>
                 </div>
                 <div className="flex justify-center animate-bounce ">
-                <a
-                    id="explore-button"
-                    href={goToTechnical}
-                    onClick={goToTechnical}
-                    className="relative cursor-pointer inline-flex items-center justify-start px-5 py-3 overflow-hidden font-bold group"
-                >
-                    <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
-                    <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
-                    <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-gray-900">
-                        Register Now
-                    </span>
-                    <span className="absolute inset-0 border-2 border-white"></span>
-                </a>
-            </div>
+                    <a
+                        id="explore-button"
+                        href={goToTechnical}
+                        onClick={goToTechnical}
+                        className="relative cursor-pointer inline-flex items-center justify-start px-5 py-3 overflow-hidden font-bold group"
+                    >
+                        <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+                        <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
+                        <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-gray-900">
+                            EXPLORE
+                        </span>
+                        <span className="absolute inset-0 border-2 border-white"></span>
+                    </a>
+                </div>
                 {/* <div className="flex flex-row gap-2 animate-bounce">
                     <button
                         id="explore-button"
@@ -1074,7 +1074,7 @@ const Home = () => {
                     />
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </motion.div>
     );
 };
