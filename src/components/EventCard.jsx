7ebdc8ee38d1818065cb0481e2cf9fb7e365ca1e
c0d3-br3ak9r <103585603+c0d3-br3ak9r@users.noreply.github.com>
2023-01-24 20:21:34 +0530

    Added glassmorphism cards

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion'
 import { useNavigate } from 'react-router-dom';
+import './GlassCard.css';
 const EventCard = ({ name, info, icon, index, description, short_description, rules, type, eligibility, organizers, link }) => {
     const uppercaseName = name !== "iCatching" ? name.toUpperCase() : "iCATCHING";
     const uppercaseInfo = info.toUpperCase();
@@ -23,6 +24,46 @@ const EventCard = ({ name, info, icon, index, description, short_description, ru
         visible: { opacity: 1, y: 0, transition: { duration: 0.6, staggerChildren: 0.2 } },
     }
 
+    /* return (
+        <div
+            id={id}
+            style={{
+                position: "relative",
+                width: "16rem",
+                minWidth: "16rem",
+                maxWidth: "16rem",
+                height: "24rem",
+                minHeight: "24rem",
+                maxHeight: "24rem",
+            }}
+            onClick={flipCard}
+            className="event-card card-both bg-gray-900 box snap-center border-4 border-blue-400 text-center mt-4 cursor-default"
+
+        >
+            <div className="box top left"></div>
+            <div className="box top right"></div>
+            <div className="box bottom left"></div>
+            <div className="box bottom right"></div>
+            <div
+                className="front-card flex flex-col items-center justify-between py-6 px-6 content"
+            >
+                <motion.div viewport={{ once: true, amount: 0.2 }} initial="hidden" whileInView="visible" variants={textAnimate} className="mb-4 place-self-center">
+                    <motion.h2 variants={textAnimate} className="font-semibold text-2xl mb-1 tracking-widest whitespace-normal ">{uppercaseName}</motion.h2>
+                    <motion.p variants={textAnimate} className="text-gray-300 font-semibold tracking-widest">{uppercaseInfo}</motion.p>
+                </motion.div>
+                <motion.img viewport={{ once: true, amount: 0.5 }} initial="hidden" whileInView="visible" variants={iconAnimate} alt="event-img" src={icon} className="w-20 my-2 self-center " />
+                <button onClick={() => navigate("/event-details", { state: { name: name, info: info, type: type, eligibility: eligibility, description: description, rules: rules, organizers: organizers, link: link } })} className="ignore-btn self-center border-solid border-2 mt-5 border-blue-400 px-4 py-2 w-fit tracking-widest transition duration-700  hover:bg-blue-400 ">
+                    MORE INFO
+                </button>
+
+            </div>
+            <div
+                className="back-card py-6 px-6 flex justify-center items-center text-center "
+            >
+                <p className="text-gray-300 font-semibold break-normal decoration-slice whitespace-pre-wrap ">{short_description}</p>
+            </div>
+        </div>
+    ) */
     return (
         <div
             id={id}
@@ -36,21 +77,18 @@ const EventCard = ({ name, info, icon, index, description, short_description, ru
                 maxHeight: "24rem",
             }}
             onClick={flipCard}
-            className="event-card card-both bg-gray-900  snap-center border-4 border-blue-400 text-center mt-4 cursor-default"
+            className="box"
 
         >
-            <div className="box top left"></div>
-            <div className="box top right"></div>
-            <div className="box bottom left"></div>
-            <div className="box bottom right"></div>
+            <span></span>
             <div
-                className="front-card flex flex-col items-center justify-between py-6 px-6"
+                className="content"
             >
-                <motion.div viewport={{ once: true, amount: 0.2 }} initial="hidden" whileInView="visible" variants={textAnimate} className="mb-4 place-self-center">
-                    <motion.h2 variants={textAnimate} className="font-semibold text-2xl mb-1 tracking-widest whitespace-normal ">{uppercaseName}</motion.h2>
-                    <motion.p variants={textAnimate} className="text-gray-300 font-semibold tracking-widest">{uppercaseInfo}</motion.p>
-                </motion.div>
-                <motion.img viewport={{ once: true, amount: 0.5 }} initial="hidden" whileInView="visible" variants={iconAnimate} alt="event-img" src={icon} className="w-20 my-2 self-center " />
+                <div viewport={{ once: true, amount: 0.2 }} initial="hidden" whileInView="visible" variants={textAnimate} className="mb-4 place-self-center">
+                    <h2 variants={textAnimate} className="font-semibold text-2xl mb-1 tracking-widest whitespace-normal ">{uppercaseName}</h2>
+                    <p variants={textAnimate} className="text-gray-300 font-semibold tracking-widest">{uppercaseInfo}</p>
+                </div>
+                <img viewport={{ once: true, amount: 0.5 }} initial="hidden" whileInView="visible" variants={iconAnimate} alt="event-img" src={icon} className="w-20 my-2 self-center " />
                 <button onClick={() => navigate("/event-details", { state: { name: name, info: info, type: type, eligibility: eligibility, description: description, rules: rules, organizers: organizers, link: link } })} className="ignore-btn self-center border-solid border-2 mt-5 border-blue-400 px-4 py-2 w-fit tracking-widest transition duration-700  hover:bg-blue-400 ">
                     MORE INFO
                 </button>
