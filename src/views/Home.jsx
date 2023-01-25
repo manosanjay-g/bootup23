2193ea7e12ea9e9ee4f45b6ad40776223ed4b4d2
Subze23 <subzesubhashsanth@gmail.com>
2023-01-25 22:00:07 +0530

    button updates

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -935,10 +935,10 @@ const Home = () => {
                         id="explore-button"
                         href={goToTechnical}
                         onClick={goToTechnical}
-                        className="relative cursor-pointer inline-flex items-center justify-start px-5 py-3 overflow-hidden font-bold group"
+                        className="rounded-md relative cursor-pointer inline-flex items-center justify-start px-5 py-3 overflow-hidden font-bold group"
                     >
-                        <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
-                        <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
+                        <span className="rounded-lg w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+                        <span className="rounded-lg absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
                         <span className="rounded-lg relative w-full text-left text-white tracking-widest transition-colors duration-200 ease-in-out group-hover:text-gray-900">
                             EXPLORE
                         </span>
