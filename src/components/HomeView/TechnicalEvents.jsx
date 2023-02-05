a86366f609a855d5c605c8f29a089455664bfcfa
KESHAVBERG <kingkeshav7@gmail.com>
2023-02-06 05:21:49 +0530

    many small changes

--- src/components/HomeView/TechnicalEvents.jsx
+++ src/components/HomeView/TechnicalEvents.jsx
@@ -12,11 +12,11 @@ const TechnicalEvents = () => {
                 "Want the world to gaze upon your project? Silicon Valley is the perfect platform to showcase your project, network with like-minded individuals, and gain valuable feedback on your projects.Participants will present their projects along with the working model and will be evaluated based on their presentation and the novelty of the project",
             short_description: "Offline Event",
             rules: [
-                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Tiki Taka, Master The Blaster.",
+                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One , Master The Blaster.",
                 "Theme of the event is Innovation/Startup.",
                 "A working model must be shown.",
                 "The product will be judged based on the concept, novelty, presentation, applicability, benefits to society, and demonstration.",
-                "The participants are required to present their project with a presentation.",
+                "The participants are required to present their project along with a presentation.",
                 "This is followed by the final demonstration of the end product. Scores are awarded based on the presentation of the team and the working model.",
                 "Results will be published at the end of the presentation session. The team that scored the maximum points will be awarded.",
                 "The Panel decision is final.",
@@ -47,9 +47,10 @@ const TechnicalEvents = () => {
                 "Ready to dive into the depths of technology and share your insights with the world? Whether you're a developer, engineer, or entrepreneur, Look no further than our upcoming paper presentation! This is your chance to showcase your hard work and share your latest findings with the world. With interactive discussions, and Q&A sessions this is an event you won't want to miss! Sign up now, and let the competition begin.",
             short_description: "Offline Event",
             rules: [
-                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Tiki Taka, Master The Blaster.",
-                "The final presentation content headings will be shared with the respective team leaders 5-6 days before the competition.",
-                "Any team will be subject to disqualification if the work is found to be plagiarised",
+                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Master The Blaster.",
+                "The final presentation Template will be shared with the respective team leaders 5-6 days before the competition.",
+                "If the work is discovered to be plagiarised, any team will be liable to disqualification",
+                "The teams are evaluated on various technical and business competencies. The top 3 teams will be rewarded.",
                 "The decision of the panel is final.",
             ],
             roundDescriptions: [
@@ -81,7 +82,7 @@ const TechnicalEvents = () => {
                 "Are you ready to put your bug-hunting skills to the test? Join our exciting technological competition and discover hidden flaws in the code. Don't miss out on this opportunity to flex your problem-solving muscles and come out on top as the ultimate bug-finding champion. Sign up now and let the hunt begin!",
             short_description: "Offline Event",
             rules: [
-                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Tiki Taka, Master The Blaster.",
+                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Master The Blaster.",
                 "Internet access will not be provided.",
                 "In case of a tie, the fastest person to debug and execute the program is deemed the winner.",
                 "The decision of the panel is final.",
@@ -115,7 +116,7 @@ const TechnicalEvents = () => {
                 "Calling all web design enthusiasts! Are you ready to put your HTML and CSS skills to the test? Join our exciting website replication competition and showcase your ability to recreate a visually stunning and responsive website design. Push the boundaries of what is possible with HTML and CSS. Don't miss out on this opportunity to flex your design muscles and come out on top as the ultimate web design champion.",
             short_description: "Offline Event",
             rules: [
-                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Tiki Taka, Master The Blaster.",
+                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Master The Blaster.",
                 "Participants must bring their own laptops.",
                 "The topics will be provided on the day of the event.",
                 "The team is expected to develop a creative front-end for the web application.",
@@ -149,13 +150,12 @@ const TechnicalEvents = () => {
                 "With lightning-fast coding rounds one after another just like a relay, this competition is designed to test your skills and push you to the limit. Each team will be given a set of coding challenges that they must solve within a given time limit, and the team that completes the most challenges with the least errors will be declared the winner. Don't miss out on the chance to be a part of the action!",
             short_description: "Offline Event",
             rules: [
-                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Tiki Taka, Master The Blaster.",
+                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Master The Blaster.",
                 "The relay order of the teammates will be collected prior and it cannot be changed once the round begins.",
                 "Only one round will be conducted for all the teams. If the question is answered before the stipulated time then the next team member can utilize the saved time for answering the next question and so on. Each question must be answered by only one teammate. ",
                 "Team members cannot help each other while solving the questions.",
-                "Malpractices will not be entertained and if the same is found then the corresponding team will be disqualified. The whole event will be organized in an auto-proctored environment.",
+                "Malpractice will not be tolerated, and if any are discovered, the corresponding teamwill be eliminated.",
                 "If a teammate is unable to complete the given question within 30 mins then the corresponding status of that question will be considered for evaluation and the next team member can proceed further.",
-                "The winners will be announced at the end of the event and the selection criteria will be based on the time they have taken to complete all the programs.",
                 "The decision taken by the panel will be final.",
             ],
             roundDescriptions: [],
@@ -184,7 +184,7 @@ const TechnicalEvents = () => {
                 "Join the elite ranks of cyber warriors in our intense Capture the Flag hacking event! Assemble your team, sharpen your skills and put your cunning and expertise to the test as you race to capture flags and outsmart your opponents. With a variety of challenging vulnerabilities to exploit, this event is the ultimate test of your hacking abilities and strategy. Will you rise to the top and claim victory? Sign up now and let the games begin!",
             short_description: "Online Event",
             rules: [
-                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Tiki Taka, Master The Blaster.",
+                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Master The Blaster.",
                 "* This event will be conducted on Feb 12, 2023 *",
                 "* Registrations for this event will be closed on Feb 9, 2023 *",
                 "This is a 24 hours event. This event starts from Feb 12,2023 12:00 AM and ends at 11:59 PM.",
@@ -221,7 +221,7 @@ const TechnicalEvents = () => {
                 "Get ready to ace your next job interview and take your career to new heights with our upcoming Mock Interview competition! This is your chance to showcase your skills, gain valuable feedback and take on the challenge of performing in a simulated real-world interview scenario. So, sharpen your skills, polish your resume and join us for an exciting and challenging competition.",
             short_description: "Offline Event",
             rules: [
-                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Tiki Taka, Master The Blaster.",
+                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Master The Blaster.",
                 "Internet access will not be provided.",
                 "Participants indulging in malpractice will be subject to disqualification.",
                 "The Panel decision is final.",
@@ -255,10 +255,10 @@ const TechnicalEvents = () => {
                 "Are you ready to flex your computer-building muscles and show off your technical prowess? This is your chance to showcase your abilities as a computer geek and prove that you know the ins and outs of a PC. With a pile of parts and a ticking clock, this competition will test your ability to assemble a PC from scratch and get it running.The participant who completes the assembly and gets the PC running the fastest will be declared the winner.",
             short_description: "Offline Event",
             rules: [
-                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Tiki Taka, Master The Blaster.",
+                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Master The Blaster.",
                 "Internet access is not allowed.",
                 "Participants indulging in malpractice will be subject to disqualification.",
-                "The Panel decision is final.",
+                "The Panel\'s decision is final.",
             ],
             roundDescriptions: [
                 "1. This will be a pen-paper round.\n\n2. The participants with the highest scores will move on to the next round.\n\n3. The duration of this event is 30 minutes.",
@@ -289,7 +289,7 @@ const TechnicalEvents = () => {
                 "Want to take automation to the next level? Come and showcase your skills in our upcoming UIPath competition! This is your chance to test your knowledge of automation and prove that you know exactly where to put the machine to maximize efficiency.Participants will be given a problem statement on the spot and will have 2 hours to solve the problem using UIPath. The team or individual whose submission has the least running time will be declared the winner.",
             short_description: "Offline Event",
             rules: [
-                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Tiki Taka, Master The Blaster.",
+                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Master The Blaster.",
                 "The participants must bring their own laptops with UiPath installed.",
                 "The problem statement will be provided at the time of the event.",
                 "Participants have to address tasks given to them and create automation for the problem given.",
@@ -324,7 +324,7 @@ const TechnicalEvents = () => {
                 "Interested to take flight and unleash the potential of drones? This is your chance to prove that you have what it takes to realize the untapped potential of drones and take it to the next level.Participants will present their drone projects and ideas, which will be judged on the basis of feasibility, creativity, code standards, and innovation. Whether it's a drone that can perform aerial acrobatics, a drone that can navigate through obstacles, or a drone that can perform a specific task, we want to see it all!",
             short_description: "Offline Event",
             rules: [
-                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Tiki Taka, Master The Blaster.",
+                "You can register and participate in a total of 6 events with the entry fee of 150 rupees. The events excluded in this entry are: Ready Player One, Master The Blaster.",
                 "Participants must bring their own drones and no additional support will be given.",
                 "A working model is only accepted.",
                 "The drone will be judged based on its design, speed, and efficiency",
