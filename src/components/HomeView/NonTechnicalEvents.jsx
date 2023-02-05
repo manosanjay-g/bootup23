75d6de4c919156459d5a48c79a83749ad524bead
keshav <49027903+KESHAVBERG@users.noreply.github.com>
2023-02-05 21:47:30 +0530

    Small changes

--- src/components/HomeView/NonTechnicalEvents.jsx
+++ src/components/HomeView/NonTechnicalEvents.jsx
@@ -68,7 +68,7 @@ const NonTechnicalEvents = () => {
                 // "1. This is an offline round.\n\n2. PCs will be provided for this round of matches.",
             ],
             type: "Team Event (Exactly 5 members + Substitutes as needed)",
-            eligibility: "1 Rounds",
+            eligibility: "Rounds varies as the teams progress.",
             organizers: [
                 {
                     name: "Dhanush Aravind A",
