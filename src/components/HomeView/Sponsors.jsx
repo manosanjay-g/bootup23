0de9eed6ac21f60980a8fef62bdc964a092984b1
Subze23 <subzesubhashsanth@gmail.com>
2023-02-10 22:39:52 +0530

    added sponsers

--- src/components/HomeView/Sponsors.jsx
+++ src/components/HomeView/Sponsors.jsx
@@ -7,21 +7,51 @@ const Sponsers = () => {
             <div className="flex flex-col items-center justify-center">
                 <h1 className="text-4xl  font-semibold tracking-widest">OUR SPONSORS</h1>
                 <div className="flex flex-col items-center md:flex-row md:justify-around w-full my-10 flex-wrap">
+                    
+                    <img
+                        src={require("../../assets/eatsure.png")}
+                        className="w-52 h-45 "
+                        alt=""
+                    />
+                    <img
+                        src={require("../../assets/dental.png")}
+                        className="w-52 h-45  "
+                        alt=""
+                    />
+                    <img
+                        src={require("../../assets/poorvika.png")}
+                        className="w-52 h-45 my-10"
+                        alt=""
+                    />
+                    <img
+                        src={require("../../assets/rosemilk.png")}
+                        className="w-52 h-45 "
+                        alt=""
+                    />
+                
+                </div>              
+                <div className="flex flex-col items-center md:flex-row md:justify-around  flex-wrap">
                     <img
                         src={require("../../assets/csiCert.png")}
-                        className="w-52 h-52  "
+                        className="w-52 h-52 mb-10"
                         alt=""
                     />
                     {/* <img
                         src={require("../../assets/ieeeCert.png")}
-                        className="w-52 h-52  "
+                        className="w-52 h-52 "
                         alt=""
                     /> */}
+                    <img
+                        src={require("../../assets/uipath.png")}
+                        className="object cover w-57 h-52 mb-10 "
+                        alt=""
+                    />
                     <img
                         src={require("../../assets/acmCert.png")}
                         className="w-52 h-52  "
                         alt=""
                     />
+                    
                 
                 </div>              
             </div>
